0825511f2dbb3d98885758b63efc77a40099b929
AlessandroMessineo <alessandromessineo46@gmail.com>
2026-03-05 18:34:32 +0100

    aggiunte rotte

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import GamePage from './pages/GamePage'
 
 function App() {
 
 
   return (
     <>
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path='/games' element={<GamePage />}/>
+          <Route path='/games/:id' element={"DetailGamePage"}/>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
